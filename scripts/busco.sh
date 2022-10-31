425473ee4ba599d88159fcc30e25aa800c6007c1
Yuqian Jiang <597174820@qq.com>
2022-10-31 21:01:08 +0800

    Update busco.sh

--- scripts/busco.sh
+++ scripts/busco.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-cd ~/data/symbio || exit
-
 # GENOMES directories
 if [ ! -d ./GENOMES ]; then
     echo 1>&2 "==> GENOMES unexist" 1>&2;
@@ -32,16 +30,21 @@ do
     if [ -f GENOMES/${file}/genome.fa ]; then
         # BUSCO processing
         echo "==>${file} BUSCO processing"
+        echo
         busco -m genome -i GENOMES/${file}/genome.fa -o ${file} \
         --out_path ./BUSCO --offline \
-        -l ~/data/symbio/busco_downloads/lineages/eukaryota_odb10 \
-        --quiet --cpu 10 --augustus
+        -l ~/data/symbio/busco_downloads/lineages/viridiplantae_odb10 \
+        --quiet --cpu 4 --augustus
         echo "==> BUSCO complete"
+
     fi
 
 
     if grep -i -q "${file}" BUSCO/busco_results.tsv; then
         echo "==> BUSCO already done"
+        echo
+        rm ./busco_*.log
+    
     elif [ -f BUSCO/${file}/short_summary*.json ]; then
         # extract info from json
         cat BUSCO/${file}/short_summary*.json |
@@ -59,14 +62,17 @@ do
             >> BUSCO/busco_results.tsv
 
             echo "==> BUSCO results summary complete"
+            echo
+
+            rm ./busco_*.log
 
             # remove all dirs and keep summary txt and json
             find BUSCO -mindepth 2 -maxdepth 2 -type d -exec rm -rf "{}" \;
 
             echo "==> ${file} empty complete"
+            echo
     else
         echo "==> BUSCO results wrong, please check"
+        echo
     fi
-
-    rm *.log
 done
