4b9f01b45d65b04af16fb712256a3684b9fdbec1
Yuqian Jiang <597174820@qq.com>
2022-12-20 03:53:41 +0800

    Update taxonomy.sh
    check nwr dependencies

--- scripts/taxonomy.sh
+++ scripts/taxonomy.sh
@@ -3,7 +3,16 @@
 usage () { echo "bash taxonomy.sh <species_name>" 1>&2; exit; }
 [ $# -lt 1 ] && usage
 
-txid=$(nwr info $1 --tsv | cut -f 1 | sed 1d)
+# Check nwr dependencies
+hash nwr 2>/dev/null || {
+    echo >&2 "nwr is required but it's not installed."
+    echo >&2 "    Install with homebrew: brew install wang-q/tap/nwr"
+}
+
+# taxonomy only accept capital
+species=$(echo $1 | sed 's/^\(.\)/\U\1/')
+
+txid=$(nwr info ${species} --tsv | cut -f 1 | sed 1d)
 
 nwr lineage $txid |
     grep -v '^no rank' |
